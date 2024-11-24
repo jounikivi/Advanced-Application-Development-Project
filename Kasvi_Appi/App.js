37d530e2e6169f7b980b75ca9c5c4aa86d6c59f2
Jouni Kiviperä <jounikivipera@gmail.com>
2024-11-24 19:22:16 +0200

    Update App.js

--- Kasvi_Appi/App.js
+++ Kasvi_Appi/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigator from "./navigation/StackNavigator"; 
+import { AppProvider } from "./context/AppProvider";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Kasvi apin aloitus</Text>
       <StatusBar style="auto" />
     </View>
   );
